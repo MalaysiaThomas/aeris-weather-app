9a863b5a960eeba39cb70c1e0bb950580d9c25e0
Malaysia Thomas <105211177+MalaysiaThomas@users.noreply.github.com>
2024-12-09 01:41:32 -0500

    style popup form and add functionality
    css styling, enter/exit functionality

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const mainContainer = document.getElementById("main-container");
 const popup = document.getElementById("myPopup");
 const userDetailSubmitButton = document.getElementById("user-detail-submit");
+const username = document.getElementById("name")
+const city = document.getElementById("city")
 
 function showPopup() {
   mainContainer.style.position = "relative";
@@ -10,17 +12,27 @@ function showPopup() {
   popup.style.display = "block";
   popup.style.zIndex = "2";
 }
-
 showPopup()
 
 function closePopup(event) {
-  event.preventDefault();
-  popup.style.display = "hidden";
-  popup.style.zIndex = "1";
-
-  mainContainer.style.position = "relative";
-  mainContainer.style.zIndex = "2";
-  mainContainer.style.filter = "blur(0)";
-}
+    if ((username.value === null || username.value === "") && (city.value === null || city.value === "")) {
+        alert("Enter your name and a city")
+    } else if (username.value === null || username.value === ""){
+        alert("Enter your name")
+    } else if (city.value === null || city.value === "")  {
+        alert("Enter a city")
+    } else {
+      event.preventDefault();
+      popup.style.display = "hidden";
+      popup.style.zIndex = "1";
+      mainContainer.style.position = "relative";
+      mainContainer.style.zIndex = "2";
+      mainContainer.style.filter = "blur(0)";
+    }
+  }
 
 userDetailSubmitButton.addEventListener("click", closePopup)
+
+
+
+
